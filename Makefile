367797d6072f1d9e1ea2fbe4c5d1cd69aeabebcd
mdw <mdw>
2018-03-02 06:48:52 +0000

    *** empty log message ***

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CURL=/opt/curl
 CURL_INC=-I$(CURL)/include
 CURL_LIBS=-L$(CURL)/lib -lcurl
-CFLAGS=$(CURL_INC)
+JSON_LIBS=-ljson-c
+CFLAGS=-g $(CURL_INC)
 doad3: doad3.o
-	$(CC) -o doad $(CURL_LIBS)
+	$(CC) -g -o doad3 doad3.o $(CURL_LIBS) $(JSON_LIBS) -lpthread
--- doad3.c
+++ doad3.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <json-c/json.h>
 
 char *my_method = "GET";
 char *my_user;
 char *my_password;
 char *my_url;
-char *my_default_url = "https://acanthodes.eng.arb.redhat.com:35357/v3";
 int multi_count;
 char *capath;
+char *my_token;
+char *my_tenant;
 int Vflag;
 
 char *
@@ -49,20 +55,9 @@ timespec_add(struct timespec *from, struct timespec *ac)
 	}
 }
 
-uint
-my_receive_http_data(char *in, uint size, uint num, void *h)
-{
-	uint r;
-	r = size * num;
-#if 0
-	fprintf(stdout,"Received: ");
-	r = fwrite(in, 1, r, stdout);
-#endif
-	return r;
-}
-
 struct curl_carrier {
 	int uses;
+	struct timespec lastuse[1];
 	CURL *h;
 };
 
@@ -86,7 +81,6 @@ get_curl_handle()
 	pthread_mutex_unlock(&saved_mutex);
 	if (ca) {
 	} else if ((h = curl_easy_init())) {
-		++carrier_allocs;
 		ca = malloc(sizeof *ca);
 		if (!ca) {
 			curl_easy_cleanup(h);
@@ -94,7 +88,6 @@ get_curl_handle()
 		}
 		ca->h = h;
 		ca->uses = 0;
-		++easy_inits_made;
 	} else {
 		// ca = 0;
 	}
@@ -109,7 +102,7 @@ release_curl_handle_now(struct curl_carrier *ca)
 }
 
 void
-release_curl_handle_now(struct curl_carrier *ca)
+release_curl_handle(struct curl_carrier *ca)
 {
 	int i;
 	if (++ca->uses > multi_count) {
@@ -144,7 +137,7 @@ void *curl_cleaner(void *h)
 		fprintf(stderr,"lock failed %d\n", errno);
 	}
 	for (;;) {
-		timespec until[1];
+		struct timespec until[1];
 		if (cleaner_shutdown) {
 			if (list_start == list_end)
 				break;
@@ -181,7 +174,7 @@ void *curl_cleaner(void *h)
 void
 init_curl_handles()
 {
-	pthread_create(&cleaner_id, NULL, curl_cleaner, nullptr);
+	pthread_create(&cleaner_id, NULL, curl_cleaner, NULL);
 }
 
 void
@@ -197,28 +190,92 @@ flush_curl_handles()
 		return;
 	}
 	if (list_start != list_end) {
-		std::cerr << "cleaner failed final cleanup" << std::endl;
+		fprintf(stderr,"cleaner failed final cleanup\n");
 	}
 }
 
+struct token_arg {
+	struct json_tokener *json_tokeniser;
+};
+
+uint
+eat_keystone_data(char *in, uint size, uint num, void *h)
+{
+	uint r;
+	r = size * num;
+	struct token_arg *a = h;
+	json_object *jobj;
+	enum json_tokener_error je;
+#if 0
+	fprintf(stdout,"Received: ");
+	r = fwrite(in, 1, r, stdout);
+#endif
+	jobj = json_tokener_parse_ex(a->json_tokeniser, in, r);
+	je = json_tokener_get_error(a->json_tokeniser);
+	if (je != json_tokener_success) {
+		fprintf(stderr,"Cannot parse json: e=%d string=<%.*s>\n",
+			r, in);
+	} else {
+		const char *response_json = json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_PRETTY);
+// if (Vflag)
+printf ("RECV: <%s>\n", response_json);
+	}
+	return r;
+}
+
+int
 get_token()
 {
 	struct curl_carrier *ca;
 	CURL *curl;
+	struct curl_slist *headers = 0;
 	CURLcode rc;
 	char error_buf[CURL_ERROR_SIZE];
 	long http_status;
+	char *temp_url = 0;
+	int i;
 	int r = 0;
-	struct receiver_arg recvarg[1];
+	struct token_arg recvarg[1];
+	json_object *jx = json_object_new_object();
+	json_object *jpu = json_object_new_object();
+	json_object *jt = json_object_new_object();
+	json_object *jm = json_object_new_array();
+	json_object *jy = json_object_new_object();
+	json_object *jz = json_object_new_object();
+	json_object *jobj = json_object_new_object();
 
+	temp_url = malloc(i = strlen(my_url) + 30);
+	snprintf(temp_url, i, "%s/auth/tokens", my_url);
+	json_object_array_add(jm, json_object_new_string("password"));
+	json_object_object_add(jx, "name", json_object_new_string("Default"));
+	json_object_object_add(jpu, "domain", jx);
+	json_object_object_add(jpu, "name", json_object_new_string(my_user));
+	json_object_object_add(jpu, "password", json_object_new_string(my_password));
+	json_object_object_add(jy, "user", jpu);
+	json_object_object_add(jt, "password", jy);
+	json_object_object_add(jt, "methods", jm);
+	json_object_object_add(jz, "identity", jt);
+	json_object_object_add(jobj, "auth", jz);
+
+	const char *req_json = json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_PLAIN);
+	recvarg->json_tokeniser = json_tokener_new();
+if (Vflag) printf ("SEND: <%s>\n", req_json);
+	headers = curl_slist_append(headers, "Content-Type: " "application/json");
+	headers = curl_slist_append(headers, "Accept: " "application/json");
+	headers = curl_slist_append(headers, "Expect:");
 	ca = get_curl_handle();
 	if (!ca) return 1;
-	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, my_method);
-	curl_easy_setopt(curl, CURLOPT_URL, my_url);
+	curl = ca->h;
+//	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, my_method);
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_URL, temp_url);
 	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
 	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
 	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error_buf);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_receive_http_data);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)recvarg);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, eat_keystone_data);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req_json);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(req_json));
 	if (Vflag) {
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 	}
@@ -233,18 +290,18 @@ get_token()
 	}
 	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_status);
 Done:
-	if (--save_until < 1) {
-		curl_easy_cleanup(curl);
-		saved_curl = 0;
-	} else {
-		saved_curl = curl;
-	}
+	release_curl_handle(ca);
+	curl_slist_free_all(headers);
+//	free(req_json);
+	if (temp_url) free(temp_url);
 	return r;
 }
 
+#define W_ADD 1
+#define W_DEL 2
 struct work_element {
-	char *op;
-	char *work;
+	int op;
+	char *what;
 	struct work_element *next;
 };
 
@@ -261,6 +318,9 @@ read_in_data()
 	char *what;
 	struct work_element *wp;
 	struct work_element **wpp;
+	int i;
+	int r = 0;
+
 	lineno = 0;
 	wpp = &work_queue;
 	while (fgets(line, sizeof line, stdin)) {
@@ -269,8 +329,8 @@ read_in_data()
 		if (cp) *cp = 0;
 		what = op = NULL;
 		for (s = line;
-			q = strtok_r(s, " \t", &ep;
-			s = NULL) {
+			(q = strtok_r(s, " \t", &ep))!= NULL;
+			s = 0) {
 			if (!op) op = q;
 			else if (!what) what = q;
 			else {
@@ -280,27 +340,34 @@ read_in_data()
 			}
 		}
 		if (!op) {
-			fprintf (stderr,"Missing op at line %d\n", lineno);
+			fprintf (stderr,"Missing op at data line %d\n", lineno);
 			return 1;
+		}
+		if (!strcmp(op, "ADD"))
+			i = W_ADD;
+		else if (!strcmp(op, "DEL"))
+			i = W_DEL;
+		else {
+			fprintf (stderr,"Bad op <%s> at data line %d\n", op, lineno);
+			r = 1;
+			continue;
 		}
 		if (!what) {
-			fprintf (stderr,"Missing filename at line %d\n", lineno);
-			return 1;
+			fprintf (stderr,"Missing arg at data line %d\n", lineno);
+			r = 1;
+			continue;
 		}
-		cp = malloc(sizeof *wp + 2 + strlen(op) + strlen(what));
+		cp = malloc(sizeof *wp + 1 + strlen(what));
 		wp = (struct work_element *) cp;
 		cp += sizeof *wp;
 		memset(wp, 0, sizeof*wp);
-		wp->op = cp;
-		i = strlen(op) + 1;
-		memcpy(cp, op, i);
-		cp += i;
+		wp->op = i;
 		wp->what = what;
 		strcpy(cp, what);
 		*wpp = wp;
 		wpp = &wp->next;
 	}
-	return 0;
+	return r;
 }
 
 void
@@ -325,7 +392,8 @@ int process()
 		fprintf(stderr,"read_in_data failed\n");
 		return r;
 	}
-	if (r = get_token()) {
+	if (my_token) {
+	} else if (r = get_token()) {
 		fprintf(stderr,"get_token failed\n");
 		return r;
 	}
@@ -335,12 +403,22 @@ int process()
 	return r;
 }
 
-int main(int ac, char **av)
+int
+main(int ac, char **av)
 {
 	char *ap, *ep, *cp;
 	char *msg;
 	int r;
-	char *change_here;
+	char *change_here = 0;
+
+	if (!my_user)
+		my_user = getenv("OS_USERNAME");
+	if (!my_password)
+		my_password = getenv("OS_PASSWORD");
+	if (!my_tenant)
+		my_tenant = getenv("OS_PROJECT_NAME");
+	if (!my_url)
+		my_url = getenv("OS_AUTH_URL");
 
 	while (--ac > 0) if (*(ap = *++av) == '-') while (*++ap) switch(*ap) {
 //	case 'v':
@@ -358,12 +436,19 @@ int main(int ac, char **av)
 	case 'V':
 		++Vflag;
 		break;
-	case 't':
+	case 'T':
 		if (ac < 1) {
 			goto Usage;
 		}
 		--ac;
 		my_token = *++av;
+		break;
+	case 't':
+		if (ac < 1) {
+			goto Usage;
+		}
+		--ac;
+		my_tenant = *++av;
 		break;
 	case 'c':
 		if (ac < 1) {
@@ -415,16 +500,21 @@ int main(int ac, char **av)
 		fprintf(stderr,"extra arg?\n");
 		goto Usage;
 	}
+	if (!my_user)
+		my_user = getenv("OS_USERNAME");
+	if (!my_password)
+		my_password = getenv("OS_PASSWORD");
+	if (!my_tenant)
+		my_tenant = getenv("OS_TENANT_NAME");
+	if (!my_url)
+		my_url = getenv("OS_AUTH_URL");
+
 	if (!change_here)
 		;
 	else if (chdir(change_here) < 0) {
 		fprintf(stderr,"Error doing chdir to %s\n", change_here);
 		exit(1);
 	}
-	if (!my_url)
-		my_url = getenv("MY_URL");
-	if (!my_url)
-		my_url = my_default_url;
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	init_curl_handles();
 	r = process();
